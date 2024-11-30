b76242c9454731202f89a69c01455b9f54e1b58d
DevanshiGoyal <goyaldevanshi444@gmail.com>
2024-11-30 22:29:15 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,101 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I’m an enthusiastic first-year student at Indira Gandhi Delhi Technical University for Women, 
+currently pursuing a B.Tech in Computer Science specializing with Artificial Intelligence. 
+My journey in tech has just begun, but I'm already excited about the possibilities ahead! 
+With a foundational knowledge in HTML, CSS, JavaScript, Python, and SQL, I’m actively building skills in web development and programming, 
+eager to explore more advanced technologies as I grow.My keen interest lies in AI , Machine Learning, CP, OpenCV, Development etc.
+My curiosity and passion for technology drive me to keep learning and adapting. 
+I thrive in environments where I can collaborate with others, 
+solve complex problems, and deliver high-quality solutions.`;
+
+export const ABOUT_TEXT = `I am deeply dedicated and passionate about exploring new technologies and continuously learning and growing. My journey began with a profound curiosity about how things work, evolving into a relentless passion for creating efficient and user-friendly web applications. I constantly strive to learn and adapt to new challenges.
+
+I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I stay active, explore new technologies,participate in hackathons and ideathons, and contribute to open-source projects. My curiosity drives me to expand my skills in AI, machine learning,Competitive Programming,computer vision, blockchain development, and UI/UX design.
+
+I have worked on impactful projects like WeCare, an app for mental health care, and innovative virtual projects using openCV and Python. As the technical coordinator for Codebenders and IEEE, I ensure effective communication and deliver results with a passion for meaningful innovation.`
+ ;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "AI-ML Mentee",
+    company: "Google Developers Group-IGDTUW.",
+    description: `As an enthusiastic member of the GDSC on-campus AI-ML circle, 
+    I am passionately committed to advancing my expertise in machine learning.
+     By engaging deeply with models such as Linear Regression, Support Vector Machines (SVM), and K-Nearest Neighbors (KNN), computer vision,
+     different ML models, I strive to master their practical applications. My journey is fueled by a desire to craft robust, efficient, and innovative ML solutions that address real-world challenges. Through hands-on projects and collaborative efforts, I am constantly refining my skills and contributing to pioneering advancements in the field of artificial intelligence. This experience not only enhances my technical abilities but also enriches my understanding of the transformative power of AI in solving complex problems.`,
+    technologies: ["Machine Learning", "Artificial Intelligence", "Regression", "Open Cv","Deep Learning","Neural Networks"],
+  },
+  {
+    year: "2024 - Present",
+    role: "Competive Programming Mentee",
+    company: "Google Developers Group-IGDTUW",
+    description: `As an eager mentee in the competitive programming circle of GDG-IGDTUW, 
+    I am zealously dedicated to honing my problem-solving and algorithmic skills.
+     By immersing myself in intricate programming challenges and algorithms, I strive to sharpen my logical thinking and coding proficiency. 
+     My journey is driven by a passion for tackling complex problems and devising efficient solutions under the guidance of experienced mentors.
+      Through collaborative coding sessions, 
+    contests, and peer learning, I am consistently enhancing my competitive programming abilities. 
+    This experience not only elevates my technical prowess but also enriches my understanding of strategic thinking and algorithm optimization, 
+    empowering me to excel in coding competitions and real-world scenarios.`,
+    technologies: ["DSA", "C++", "LeetCode", "Codechef","DP"],
+  },
+  {
+    year: "2024 - Present",
+    role: "Web Development and Content Coordinator",
+    company: "IEEE-IGDTUW",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. As a member of the Web Admins team at IEEE IGDTUW, I’ve gained valuable insights into open-source contributions and enhanced my skills in frontend development using React.js. 
+    This role has provided me with hands-on experience in web development while deepening my understanding of collaborative tech projects.
+    Also managed and curated content related to technologies,science and innovations.`,
+    technologies: ["HTML", "CSS", "React.js", "Next.js", "Javascript"],
+  },
+  {
+    year: "2024 - Present",
+    role: "Technical Coordinator",
+    company: "CodeBenders-IGDTUW",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. 
+    Managed databases and implemented data storage solutions using SQL.Honing my Peoblem Solving skills and strenghtening my DSA concepts`,
+    technologies: ["Teamwork", "Planning", "Communication-skills", "Problem-Solving"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Delhi-110006",
+  phoneNo: "+917302158156",
+  email: "devanshi052btcseai24@igdtuw.ac.in",
+};
